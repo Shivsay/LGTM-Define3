47da4eb48f6afab317916f82691c9e71c638e657
rehanphilipjacob <rehanphilipjacob@gmail.com>
2025-03-08 21:59:59 +0530

    Added AddAircraftPage

--- frontend/define/src/components/Footer.jsx
+++ frontend/define/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-<footer class="bg-white rounded-lg shadow-sm dark:bg-gray-900 m-4 fixed bottom-0 left-0 w-full z-50">
+<footer class="bg-white rounded-lg shadow-sm dark:bg-gray-900 w-full z-50 mt-auto">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
---
+++ frontend/define/src/pages/AddAircraftPage.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// import { toast } from 'react-toastify';
+
+const AddAircraftPage = ({ addAircraftSubmit }) => {
+  const [registration, setRegistration] = useState('');
+  const [aircrafttype, setAircraftType] = useState('');
+  const [seatingcapacity, setSeatingCapacity] = useState('');
+
+  const navigate = useNavigate();
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const newAircraft = {
+      aircrafttype:aircrafttype,
+      registration: registration,
+      seatingcapacity: seatingcapacity
+    };
+
+    addAircraftSubmit(newAircraft);
+
+    // toast.success('Aircraft Added Successfully');
+
+    return navigate('/dashboard');
+  };
+
+  return (
+    <section className='bg-indigo-50'>
+      <div className='container m-auto max-w-2xl py-24'>
+        <div className='bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0'>
+          <form onSubmit={submitForm}>
+            <h2 className='text-3xl text-center font-semibold mb-6'>Add Aircraft</h2>
+
+            <div className='mb-4'>
+              <label className='block text-gray-700 font-bold mb-2'>
+                Aircraft Registration
+              </label>
+              <input
+                type='text'
+                id='registration'
+                name='registration'
+                className='border rounded w-full py-2 px-3 mb-2'
+                placeholder=''
+                required
+                value={registration}
+                onChange={(e) => setRegistration(e.target.value)}
+              />
+            </div>
+
+             <div className='mb-4'>
+              <label className='block text-gray-700 font-bold mb-2'>
+              </label>
+              <input
+                type='text'
+                id='aircrafttype'
+                name='aircrafttype'
+                className='border rounded w-full py-2 px-3 mb-2'
+                placeholder=''
+                required
+                value={aircrafttype}
+                onChange={(e) => setAircraftType(e.target.value)}
+              />
+            </div>
+          
+            <div className='mb-4'>
+              <label className='block text-gray-700 font-bold mb-2'>
+                Seating Capacity
+              </label>
+              <input
+                type='text'
+                id='seatingcapacity'
+                name='seatingcapacity'
+                className='border rounded w-full py-2 px-3 mb-2'
+                placeholder='Company Location'
+                required
+                value={seatingcapacity}
+                onChange={(e) => setSeatingCapacity(e.target.value)}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default AddAircraftPage;
